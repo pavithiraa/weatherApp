524ed92fe633a389f76f2f88173a768c6c5ed7a1
pavithiraa <hrpavithraa2000@gmail.com>
2022-01-23 17:10:16 +0530

    My Weather App

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "8bb820537fcaf843924812c43fa530e3"
+    }
+}
+
+module.exports = constants;
